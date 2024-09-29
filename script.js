c9a69cb2108a410f427e0d1c35632b55c6c97cb6
Aleksandr Barabanov <alex.de.barabanov@yandex.ru>
2024-09-29 17:59:06 +0200

    Last Version

--- script.js
+++ script.js
@@ -27,21 +27,17 @@ const click = document.querySelector(".click");
 const inputField = document.querySelector(".input-field");
 
 function fadeInOut() {
-  responseDiv.style.opacity = 0; // Начальная прозрачность
-
-  // Скрываем текст с анимацией
+  responseDiv.style.opacity = 0;
 
   responseDiv.animate([{ opacity: 1 }, { opacity: 0 }], {
-    duration: 700, // Продолжительность анимации в миллисекундах
-    fill: "forwards", // Завершаем анимацию с последним кадром
+    duration: 400,
+    fill: "forwards",
   });
-
-  // Показываем текст снова через 2.3 секунды
 
   setTimeout(() => {
     responseDiv.animate([{ opacity: 0 }, { opacity: 1 }], {
-      duration: 1000, // Продолжительность анимации в миллисекундах
-      fill: "forwards", // Завершаем анимацию с последним кадром
+      duration: 1000,
+      fill: "forwards",
     });
   }, 2380);
 }
@@ -49,67 +45,51 @@ function fadeInOut() {
 const showElement = () => {
   const randomElement = variants[Math.floor(Math.random() * variants.length)];
 
-  // Удаляем класс, если он уже есть
-
   ballElement.classList.remove("animate");
 
-  // Добавляем класс для запуска анимации запуска функции исчезновения текста, чтобы к концу отрисовки был уже новый
- 
   setTimeout(() => {
     ballElement.classList.add("animate");
-    setTimeout(fadeInOut, 10); // Добавляем задержку перед вызовом fadeInOut()
+    setTimeout(fadeInOut, 10);
     inputField.value = "";
-  }, 10); // Задержка в миллисекундах (можно изменить)
+  }, 10);
 
-  //  Вывод ответа с задержкой для совпадения с последним  кадром
-  
   setTimeout(() => {
     responseDiv.textContent = randomElement;
-
-    // Добавление вывода ответа для screenreader
-  
     responseDiv.setAttribute("aria-live", "polite");
   }, 2250);
 };
 
 inputField.addEventListener("keydown", function (event) {
   if (event.key === "Enter") {
-    event.preventDefault(); // Предотвращаем стандартное действие браузера и сброс отрисовки так как работало обновление страницы как submit ))) Баг исправлен.
+    event.preventDefault();
     showElement();
   }
 });
 
-//  По клику запускаем всё))))
-
-button.addEventListener("click", showElement);
-
-click.addEventListener("click", showElement);
-
-// Добавляем реакцию на взаимодействие при использовании мобильных и планшетов
-
-const form = document.querySelector(".form");
-
-// Для загрузки ответа после ввода в поле input
-// Обработчик события submit для формы
-
-form.addEventListener("submit", function (event) {
-  event.preventDefault(); // Предотвращаем стандартное действие отправки формы
-
-  // Здесь вы можете добавить необходимую логику для обработки отправки формы, например, вызов функции showElement()
+button.addEventListener("click", function (event) {
+  event.preventDefault();
   showElement();
 });
 
-// Функция для обработки события нажатия на кнопку или магический шар
+click.addEventListener("click", function (event) {
+  event.preventDefault();
+  showElement();
+});
+
+const form = document.querySelector(".form");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  showElement();
+});
 
 function handleTouchStart(event) {
-  event.preventDefault(); // Предотвращаем стандартное действие браузера
-  showElement(); // Вызываем функцию для отображения результата
+  event.preventDefault();
+  showElement();
 }
-
-// Добавляем обработчик события касания для кнопки
 
 button.addEventListener("touchstart", handleTouchStart);
 
-// Добавляем обработчик события касания для магического шара
+const magicBall = document.querySelector(".magic-ball-container");
 
 magicBall.addEventListener("touchstart", handleTouchStart);
